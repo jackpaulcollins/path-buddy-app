a4b3b30b5ee290a9ae6606b441b1f13346d6b748
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2023-08-23 15:52:37 -0700

    get a dummy redux config going

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from './auth/authSlice';
 
 function App() {
+  const currentUser = useSelector((state) => state.auth.currentUser )
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          { currentUser }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
