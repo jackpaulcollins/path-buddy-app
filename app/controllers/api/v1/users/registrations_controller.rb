6a5f8b80c2dd41ae71ed34b4759cd4d78294e8ac
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2023-08-24 20:24:19 -0700

    get token passed back and set state with it

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -2,15 +2,24 @@ module Api
   module V1
     module Users
       class RegistrationsController < Devise::RegistrationsController
+        include RackSessionFix
         respond_to :json
 
-        def create
-          build_resource(sign_up_params)
-          resource.save
-          if resource.save
-            render json: { user: resource  }
+
+        def respond_with(resource, _opts = {})
+          if request.method == "POST" && resource.persisted?
+            render json: {
+              status: {code: 200, message: "Signed up sucessfully."},
+              data: { user: resource.email }
+            }, status: :ok
+          elsif request.method == "DELETE"
+            render json: {
+              status: { code: 200, message: "Account deleted successfully."}
+            }, status: :ok
           else
-            render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+            render json: {
+              status: {code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
+            }, status: :unprocessable_entity
           end
         end
 
