79402c479a225f5b47b8948280fa7ac15f1dabce
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2023-08-25 15:13:24 -0700

    eslin

--- client/src/features/auth/Welcome.jsx
+++ client/src/features/auth/Welcome.jsx
@@ -1,21 +1,24 @@
-import { useSelector } from "react-redux"
-import { selectCurrentUser, selectCurrentToken } from "./authSlice"
-import { Link } from "react-router-dom"
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { selectCurrentUser, selectCurrentToken } from './authSlice';
 
-const Welcome = () => {
-    const user = useSelector(selectCurrentUser)
-    const token = useSelector(selectCurrentToken)
+function Welcome() {
+  const user = useSelector(selectCurrentUser);
+  const token = useSelector(selectCurrentToken);
 
-    const welcome = user ? `Welcome ${user}!` : 'Welcome!'
-    const tokenAbbr = `${token.slice(0, 9)}...`
+  const welcome = user ? `Welcome ${user}!` : 'Welcome!';
+  const tokenAbbr = `${token.slice(0, 9)}...`;
 
-    const content = (
-        <section className="welcome">
-            <h1>{welcome}</h1>
-            <p>Token: {tokenAbbr}</p>
-        </section>
-    )
+  const content = (
+    <section className="welcome">
+      <h1>{welcome}</h1>
+      <p>
+        Token:
+        {tokenAbbr}
+      </p>
+    </section>
+  );
 
-    return content
+  return content;
 }
-export default Welcome
+export default Welcome;
