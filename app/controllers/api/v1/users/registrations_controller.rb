bb1c147ad500f9ff669e7c8b0ba9bf4b846e5f80
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2023-08-25 21:44:48 -0700

    run rubocop

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     module Users
@@ -5,20 +7,19 @@ module Api
         include RackSessionFix
         respond_to :json
 
-
         def respond_with(resource, _opts = {})
-          if request.method == "POST" && resource.persisted?
+          if request.method == 'POST' && resource.persisted?
             render json: {
-              status: {code: 200, message: "Signed up sucessfully."},
+              status: { code: 200, message: 'Signed up sucessfully.' },
               data: { user: resource.email }
             }, status: :ok
-          elsif request.method == "DELETE"
+          elsif request.method == 'DELETE'
             render json: {
-              status: { code: 200, message: "Account deleted successfully."}
+              status: { code: 200, message: 'Account deleted successfully.' }
             }, status: :ok
           else
             render json: {
-              status: {code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
+              status: { code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
             }, status: :unprocessable_entity
           end
         end
--- app/models/concerns/rack_session_fix.rb
+++ app/models/concerns/rack_session_fix.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module RackSessionFix
   extend ActiveSupport::Concern
   class FakeRackSession < Hash
@@ -7,7 +9,9 @@ module RackSessionFix
   end
   included do
     before_action :set_fake_rack_session_for_devise
+
     private
+
     def set_fake_rack_session_for_devise
       request.env['rack.session'] ||= FakeRackSession.new
     end
