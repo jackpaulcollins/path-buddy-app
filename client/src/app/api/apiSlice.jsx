3d7af7f73bfcf2f96d6fd4b4da80a7969ea9cced
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2023-08-25 22:01:44 -0700

    more eslint

--- client/src/app/api/apiSlice.jsx
+++ client/src/app/api/apiSlice.jsx
@@ -1,46 +1,49 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { setCredentials, logout } from '../../features/auth/authSlice'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { setCredentials, logout } from '../../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3000',
-    credentials: 'include',
-    prepareHeaders: (headers, { getState }) => {
-        const token = getState().auth.token
-        if (token) {
-            headers.set("authorization", `Bearer ${token}`)
-        }
-        return headers
-    },
-    responseHandler: async (response) => {
-        const data = await response?.json();
-        const headers = {
-            'content-type': response.headers.get('content-type'),
-            authorization: response.headers.get('authorization'),
-        };
-    
-        return { data, headers };
+  baseUrl: 'http://localhost:3000',
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const { token } = getState().auth;
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
     }
-})
+    return headers;
+  },
+  responseHandler: async (response) => {
+    const data = await response?.json();
+    const headers = {
+      'content-type': response.headers.get('content-type'),
+      authorization: response.headers.get('authorization'),
+    };
+
+    return { data, headers };
+  },
+});
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
-    let result = await baseQuery(args, api, extraOptions)
-    if (result?.error?.originalStatus === 403) {
-        console.log('sending refresh token')
-        const refreshResult = await baseQuery('/refresh', api, extraOptions)
-        console.log(refreshResult)
-        if (refreshResult?.data) {
-            const user = api.getState().auth.user
-            api.dispatch(setCredentials({ ...refreshResult.data, user }))
-            result = await baseQuery(args, api, extraOptions)
-        } else {
-            api.dispatch(logout())
-        }
+  let result = await baseQuery(args, api, extraOptions);
+  if (result?.error?.originalStatus === 403) {
+    console.log('sending refresh token');
+    const refreshResult = await baseQuery('/refresh', api, extraOptions);
+    console.log(refreshResult);
+    if (refreshResult?.data) {
+      const { user } = api.getState().auth;
+      api.dispatch(setCredentials({ ...refreshResult.data, user }));
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
     }
+  }
 
-    return result
-}
+  return result;
+};
 
-export const apiSlice = createApi({
-    baseQuery: baseQueryWithReauth,
-    endpoints: builder => ({})
-})
+const apiSlice = createApi({
+  baseQuery: baseQueryWithReauth,
+  // eslint-disable-next-line no-unused-vars
+  endpoints: (builder) => ({}),
+});
+
+export default apiSlice;
