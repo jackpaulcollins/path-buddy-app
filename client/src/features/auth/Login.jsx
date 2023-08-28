ebf74132fbae6daba70f71f38a3830b4ad51bf82
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2023-08-27 20:44:34 -0700

    remove remember me options

--- client/src/features/auth/Login.jsx
+++ client/src/features/auth/Login.jsx
@@ -104,17 +104,6 @@ function Login() {
             </div>
 
             <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <label htmlFor="remember-me">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                  />
-                </label>
-                <p className="ml-3 block text-sm leading-6 text-gray-900">Remember me</p>
-              </div>
 
               <div className="text-sm leading-6">
                 <Link to="/register" className="font-semibold text-indigo-600 hover:text-indigo-500">
--- client/src/features/auth/Register.jsx
+++ client/src/features/auth/Register.jsx
@@ -194,17 +194,6 @@ function Login() {
             </div>
 
             <div className="flex items-center justify-between">
-              <div className="flex items-center justify-evenly">
-                <label htmlFor="remember-me">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                  />
-                </label>
-                <p className="ml-3 block text-sm leading-6 text-gray-900">Remember me</p>
-              </div>
 
               <div className="text-sm leading-6">
                 <Link to="/login" className="font-semibold text-indigo-600 hover:text-indigo-500">
