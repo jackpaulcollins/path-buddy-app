3a02c4d84496d29fd3bca9ae6999240b24b405af
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2023-08-29 16:29:46 -0700

    prepare for actual verify

--- client/src/features/auth/RequireAuth.jsx
+++ client/src/features/auth/RequireAuth.jsx
@@ -1,13 +1,28 @@
+/* eslint-disable no-console */
 import { useLocation, Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectCurrentToken } from './authSlice';
 
 function RequireAuth() {
-  const token = useSelector(selectCurrentToken);
   const location = useLocation();
 
+  const verifyToken = () => {
+    const token = useSelector(selectCurrentToken);
+    if (token) {
+      // verify
+
+      console.log('true');
+      return true;
+    }
+
+    console.log('false');
+    return false;
+  };
+
+  const authenticated = !!verifyToken();
+
   return (
-    token
+    authenticated
       ? <Outlet />
       : <Navigate to="/public" state={{ from: location }} replace />
   );
