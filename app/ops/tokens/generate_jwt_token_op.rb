3e644c6d62b534c26104bd6c82e2b4c37e6182cd
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2023-09-02 19:52:36 -0700

    turn the expiry back to 4hrs

--- app/ops/tokens/generate_jwt_token_op.rb
+++ app/ops/tokens/generate_jwt_token_op.rb
@@ -16,7 +16,7 @@ module Tokens
     end
 
     def generate_jwt_token(user)
-      exp_payload = { data: user.id, jti: user.jti, exp: Time.now.to_i + 60 }
+      exp_payload = { data: user.id, jti: user.jti, exp: Time.now.to_i + 4 * 3600 }
       s = ENV['JWT_SECRET_KEY']
       a = ENV['JWT_ALGORITHM']
       JWT.encode exp_payload, s, a
