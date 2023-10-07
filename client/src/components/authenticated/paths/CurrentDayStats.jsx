eed2715ac31131938f49604dcc83622c0956e697
Jack Collins <jackpaulcollins@gmail.com>
2023-10-06 17:58:22 -0700

    add icons for date nav

--- client/src/components/authenticated/paths/CurrentDayStats.jsx
+++ client/src/components/authenticated/paths/CurrentDayStats.jsx
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
 import Check from '../../../assets/icons/Check';
+import LeftCarrot from '../../../assets/icons/LeftCarrot';
+import RightCarrot from '../../../assets/icons/RightCarrot';
 
 function CurrentDayStats({ date }) {
   CurrentDayStats.propTypes = {
     date: PropTypes.string.isRequired,
   };
   return (
-    <div className="inline-flex">
+    <div className="inline-flex w-full justify-evenly">
+      <div><LeftCarrot /></div>
       <Check extraClasses="text-green-600 text-l" />
       <h1 className="font-semibold">{date}</h1>
+      <div><RightCarrot /></div>
     </div>
   );
 }
