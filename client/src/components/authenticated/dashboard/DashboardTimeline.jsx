484dd239d2f4e34c17787730272f9656bc043d37
Jack Collins <jackpaulcollins@gmail.com>
2023-10-10 15:14:21 -0700

    weird

--- client/src/components/authenticated/dashboard/DashboardTimeline.jsx
+++
@@ -1,9 +0,0 @@
-function DashboardTimeline() {
-  return (
-    <div>
-      Hello Dashboard Timeline
-    </div>
-  );
-}
-
-export default DashboardTimeline;
--- client/src/constants/timeZones.js
+++
@@ -1,8 +0,0 @@
-const systemTimeZones = [
-  { label: 'Pacific Time (US & Canada)', value: 'Pacific Time (US & Canada)' },
-  { label: 'Mountain Time (US & Canada)', value: 'Mountain Time (US & Canada)' },
-  { label: 'Central Time (US & Canada)', value: 'Central Time (US & Canada)' },
-  { label: 'Eastern Time (US & Canada)', value: 'Eastern Time (US & Canada)' },
-];
-
-export default systemTimeZones;
