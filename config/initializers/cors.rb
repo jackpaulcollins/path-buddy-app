7ba2088d50ceb29f2a810f78561bfd665cfab063
Jack Collins <jackpaulcollins@gmail.com>
2023-10-10 15:24:57 -0700

    remove trailing slash

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://pb-client-six.vercel.app/"
+    origins "http://localhost:3000", "https://pb-client-six.vercel.app"
 
     resource '/api/v1/*',
       headers: :any,
