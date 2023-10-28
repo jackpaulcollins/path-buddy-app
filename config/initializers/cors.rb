97b9b55aa3b8fe551f10895ec0ec5af43a2d31b0
Jack Collins <jackpaulcollins@gmail.com>
2023-10-28 14:12:17 -0700

    change domain for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://pb-client-six.vercel.app"
+    origins 'http://localhost:3000', 'https://path-buddy-client.vercel.app'
 
     resource '/api/v1/*',
-      headers: :any,
-      expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+             headers: :any,
+             expose: %w[access-token expiry token-type Authorization],
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
