6fdb7f0aada0527d04213f16c59c1021525d3a23
Jack Collins <jackpaulcollins@gmail.com>
2023-10-28 23:30:54 -0700

    don't use https local

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://localhost:3000', 'https://path-buddy-client.vercel.app'
+    origins 'http://localhost:3000', 'https://path-buddy-client.vercel.app'
 
     resource '/api/v1/*',
              headers: :any,
